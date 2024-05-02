6a040e41991a43cdec5fd6022ab25a8c1c880c5b
Hycarus <mirkomarasco08@gmail.com>
2024-05-02 11:36:05 +0200

    small fix

--- ProgrammaEventi.cs
+++ ProgrammaEventi.cs
@@ -1,11 +1,63 @@
 ﻿using System;
+using System.Diagnostics.Tracing;
+
 namespace GestoreEventi
 {
 	public class ProgrammaEventi
 	{
-		public ProgrammaEventi()
+		public string Title { get; set; }
+		public static List<Event> Events { get; set; }
+
+		public ProgrammaEventi(string title)
 		{
+			Events = new List<Event>();
+			title = Title; 
 		}
-	}
+
+        public void AddNewEvent(Event evento)
+        {
+            Events.Add(evento);
+        }
+
+		public List<Event> ShowEventsByDate(DateTime date)
+		{
+			List<Event> EventsOnDate = new List<Event>();
+			foreach(var evento in Events)
+			{
+				if(date == evento.Date)
+				{
+					EventsOnDate.Add(evento);
+				}
+			}
+			return EventsOnDate;
+		}
+
+		public static string ShowEventsList()
+		{
+			string path = "";
+			foreach(var evento in Events)
+			{
+				path += $"\n{evento.Date} - {evento.Title}";
+			}
+			return path;
+		}
+
+		public int HowManyEvents()
+		{
+			return Events.Count();
+		}
+
+		public void DeleteAllEvents()
+		{
+			Events.Clear();
+		}
+
+		public string ShowProgramTitleAndEvents()
+		{
+			return $"{Title}{ShowEventsList()}";
+		}
+    }
+
+	
 }
 
